614168536bee3e0d40fc4c0e5971161b7e965303
huzhaoyang <huchaoyang>
2017-11-22 19:07:10 +0800

    add a vue web project

---
+++ src/components/Login.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="login">
+      <div class= "login-content">
+          <input type = "text" class = "input-style" v-model = "loginUser"/>
+          <input type = "password" class = "input-style"  v-model = "loginPassword">
+           <button @click.native="loginmeth">{{loginAction}}</button>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      loginAction: '登录',
+      loginUser: '',
+      loginPassword: ''
+    }
+  },
+  method:{
+    loginmeth () {
+      console.log("err");
+    },
+   re(){
+      console.log("err");
+   }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
