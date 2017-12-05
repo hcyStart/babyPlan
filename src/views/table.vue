fd8f387e0d704ffd920a5708c3213ec145a5dfeb
huzhaoyang <huchaoyang>
2017-12-05 19:09:39 +0800

    优化列白哦

--- src/views/table.vue
+++ src/views/table.vue
@@ -1,47 +1,151 @@
+<style scoped lang="less">
+  .ivu-page-prev{
+     border: none;
+  }
+  .ivu-form-item{
+    width : 250px;
+  }
+</style>
 <template>
-    <Table border :columns="columns7" :data="data6"></Table>
+  <div>
+      <Form ref="queryData" :model="queryData" :rules="ruleValidate" :label-width="80"    inline>
+          <FormItem label="业务编号" prop="wordID">
+              <Input type="text" v-model="queryData.wordID"></Input>
+          </FormItem>
+          <FormItem label="组织类别" prop="orgniseType">
+            <Select v-model="queryData.orgniseType">
+                <Option value="beijing">beijing</Option>
+                <Option value="shanghai">shanghai</Option>
+                <Option value="shenzhen">shenzhen</Option>
+            </Select>
+          </FormItem>
+          <FormItem label="提交时间" prop="submitDate">
+              <DatePicker type="date" placeholder="请选择..." v-model="queryData.submitDate"></DatePicker>
+          </FormItem>
+          <FormItem label="完成时间" prop="finishDate">
+              <DatePicker type="date" placeholder="请选择..." v-model="queryData.finishDate"></DatePicker>
+          </FormItem>
+          <FormItem  :label-width="80">
+              <Button type="primary" @click="handleSubmit('queryData')">搜索</Button>
+              <Button type="ghost" @click="handleReset('queryData')" style="margin-left: 10px;border:none;color: blue;font-size: 12px;">高级搜索</Button>
+          </FormItem>
+      </Form>
+     <Table stripe border :columns="tableHeader" :data="tableData"></Table>
+      <div style="margin: 10px;overflow: hidden">
+          <div style="float: right;">
+              <Page :total="100" :current="1" @on-change="changePage" ></Page>
+          </div>
+      </div>
+    </div>
 </template>
 <script>
     export default {
         data () {
             return {
-                columns7: [
+            queryData: {
+                    name: '',
+                    wordID: '',
+                    orgniseType: '',
+                    submitDate: '',
+                    finishDate: ''
+                },
+                ruleValidate: {
+                  wordID: [
+                          { required: true, message: 'The field cannot be empty', trigger: 'blur' }
+                        ]
+                },
+                tableData: this.creatTableData(),
+                tableHeader: [
                     {
                         title: '序号',
                         key: 'idNo',
                     },
                     {
                         title: '业务编号',
-                        key: 'age'
+                        key: 'wordNo',
+                        render: (h, params) => {
+                            const row = params.row;
+                            const text = row.wordNo == 1 ? 'wwww.baidu.com' : row.wordNo == 1 ? 'www.bai.com': 'www.du.com';
+                            return h('div', {
+                                style: {
+                                    textAlign: 'center',
+                                    color: 'blue',
+                                    size: '15',
+                                    'text-decoration': 'underline',
+                                    cursor: 'pointer'
+                                },
+                                on: {
+                                    click: () => {
+                                        this.show(params.index)
+                                    }
+                                }
+                            }, text);
+                        }
                     },
                     {
                         title: '组织编号',
-                        key: 'address'
+                        key: 'orgNo',
+                        render: (h, params) => {
+                          const text = params.row.orgNo;
+                            return h('div', {
+                                style: {
+                                  textAlign: 'center',
+                                  size: '15'
+                                }
+                            },text);
+                        }
                     },
                     {
                         title: '组织名称',
-                        key: 'address'
+                        key: 'orgname',
+                        render: (h, params) => {
+                          const text = params.row.orgname;
+                            return h('div', {
+                                style: {
+                                  textAlign: 'center',
+                                  size: '15'
+                                }
+                            },text);
+                        }
                     },
                     {
                         title: '组织类别',
-                        key: 'address'
+                        key: 'orgtype',
+                        render: (h, params) => {
+                          const text = params.row.orgtype;
+                            return h('div', {
+                                style: {
+                                  textAlign: 'center',
+                                  size: '15'
+                                }
+                            },text);
+                        }
                     },
                     {
                         title: '提交时间',
-                        key: 'address'
+                        key: 'submitTime',
+                        render: (h, params) => {
+                            return h('div', this.formateDate(this.tableData[params.index].submitTime));
+                        }
                     },
                     {
                         title: '完成时间',
-                        key: 'address'
+                        key: 'finishTime',
+                        render: (h, params) => {
+                            return h('div', this.formateDate(this.tableData[params.index].finishTime));
+                        }
                     },
                     {
                         title: '操作者',
-                        key: 'address'
+                        key: 'opreater',
+                        render: (h, params) => {
+                            return h('div', this.checkUser(this.tableData[params.index].opreater));
+                        }
                     },
                     {
                         title: '编辑',
                         key: 'action',
-                        width: 50,
+                        width: 80,
                         align: 'center',
                         render: (h, params) => {
                          return h('div', [
@@ -52,7 +156,7 @@
                                      color: 'blue'
                                  },
                                  style: {
-                                     marginRight: '5px'
+                                     cursor: 'pointer'
                                  },
                                  nativeOn: {
                                      click: () => {
@@ -67,43 +171,65 @@
                         type: 'selection',
                         width: 60,
                         align: 'center'
-                    },
-                ],
-                data6: [
-                    {
-                        idNo: '1',
-                        name: 'John Brown',
-                        age: 18,
-                        address: 'New York No. 1 Lake Park'
-                    },
-                    {
-                        idNo: '1',
-                        name: 'Jim Green',
-                        age: 24,
-                        address: 'London No. 1 Lake Park'
-                    },
-                    {
-                        idNo: '1',
-                        name: 'Joe Black',
-                        age: 30,
-                        address: 'Sydney No. 1 Lake Park'
-                    },
-                    {
-                       idNo: '1',
-                        name: 'Jon Snow',
-                        age: 26,
-                        address: 'Ottawa No. 2 Lake Park'
                     }
                 ]
             }
         },
         methods: {
+            creatTableData () {
+                let data = [];
+                for (let i = 0; i < 10; i++) {
+                    data.push({
+                        idNo: Math.floor(Math.random () * 5 + 1),
+                        wordNo:  Math.floor(Math.random () * 3 + 1),
+                        orgNo:  Math.floor(Math.random () * 1000000 + 100000),
+                        orgname: 'People' + Math.floor(Math.random () * 100 + 1),
+                        orgtype: 'type' + Math.floor(Math.random () * 100 + 1),
+                        time: Math.floor(Math.random () * 7 + 1),
+                        submitTime: new Date().getTime(),
+                        finishTime: new Date().getTime()+(7*24*60*60*1000),
+                        opreater: Math.floor(Math.random () * 5)
+                    })
+                }
+                return data;
+            },
+            formateDate (date) {
+                date = new Date(date);
+                const y = date.getFullYear();
+                let m = date.getMonth() + 1;
+                m = m < 10 ? '0' + m : m;
+                let d = date.getDate();
+                d = d < 10 ? ('0' + d) : d;
+                return y + '-' + m + '-' + d;
+            },
+            checkUser(opreaterID){
+                let  opreaterArray = ['tom','jame','sunny','shine','bob','alice'];
+                return opreaterArray[opreaterID];
+            },
+            changePage () {
+                // The simulated data is changed directly here, and the actual usage scenario should fetch the data from the server
+                this.tableData = this.creatTableData();
+            },
             show (index) {
-            debugger;
                 this.$Modal.info({
                     title: 'User Info',
-                    content: `Name：${this.data6[index].name}<br>Age：${this.data6[index].age}<br>Address：${this.data6[index].address}`
+                    content: `Name：${this.tableData[index].idNo}<br>Age：${this.tableData[index].wordNo}<br>Address：${this.tableData[index].wordNo}`
                 })
+            },
+            handleSubmit (name) {
+                this.$refs[name].validate((valid) => {
+                    if (valid) {
+                      this.$Modal.info({
+                          title: '筛选条件',
+                          content: `组织类型：${this.queryData.orgniseType}<br>提交时间：${this.queryData.submitDate}<br>完成时间：${this.queryData.finishDate}`
+                      })
+                    } else {
+                        this.$Message.error('Fail!');
+                    }
+                })
+            },
+            handleReset (name) {
+                this.$refs[name].resetFields();
             }
         }
     }
